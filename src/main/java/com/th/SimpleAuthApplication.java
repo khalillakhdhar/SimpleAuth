eed12564b08ad3e71eb06aeceb6cab2140a4ee86
khalil <khalillakhdharatc@gmail.com>
2022-03-28 14:20:07 +0100

    auth

--- src/main/java/com/th/SimpleAuthApplication.java
+++ src/main/java/com/th/SimpleAuthApplication.java
@@ -6,23 +6,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.th.dao.TaskRepository;
 import com.th.entities.Task;
+import com.th.service.AccountServiceImplement;
 
 @SpringBootApplication
 public class SimpleAuthApplication implements CommandLineRunner {
 
 	@Autowired
 	TaskRepository taskRepository;
-	
+	@Autowired
+	AccountServiceImplement accountServiceImplement;
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleAuthApplication.class, args);
 	}
+	
+	@Bean
+	// avoir un BCryptEncoder qui est initialisé une et une seule fois
+	public BCryptPasswordEncoder getBCPE()
+	{
+		return new BCryptPasswordEncoder();
+		
+	}
 
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		
+		
 		Stream.of("T1","T2","T3").forEach(t->{
 			taskRepository.save(new Task(t));
 			});
--- src/main/java/com/th/service/AccountServiceImplement.java
+++ src/main/java/com/th/service/AccountServiceImplement.java
@@ -1,32 +1,55 @@
 package com.th.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.th.dao.RoleRepository;
+import com.th.dao.UserRepository;
 import com.th.entities.AppRole;
 import com.th.entities.AppUser;
 
+@Service
+@Transactional
 public class AccountServiceImplement implements AccountService {
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired 
+	RoleRepository roleRepository;
+	
 	@Override
 	public AppUser saveUser(AppUser appUser) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		String hashPW=bCryptPasswordEncoder.encode(appUser.getPassword()); //hashPW = le mot de passe crypté
+		System.out.print(hashPW);
+		appUser.setPassword(hashPW);
+		return userRepository.save(appUser);
 	}
 
 	@Override
 	public AppRole saveRole(AppRole role) {
 		// TODO Auto-generated method stub
-		return null;
+		return roleRepository.save(role);
 	}
 
 	@Override
 	public void addRoleToUser(String username, String roleName) {
 		// TODO Auto-generated method stub
-
+		AppRole role=roleRepository.findByRoleName(roleName);
+		AppUser user=userRepository.findByUsername(username);
+		user.getRoles().add(role);
 	}
 
 	@Override
 	public AppUser findUserByUserName(String username) {
 		// TODO Auto-generated method stub
-		return null;
+		return userRepository.findByUsername(username);
 	}
 
 }
